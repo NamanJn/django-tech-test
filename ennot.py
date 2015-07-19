07507775b2a587e3b7cbcac6d4923a4d430c7499
Naman Jain <nj1411@ic.ac.uk>
2015-07-19 16:25:38 +0100

    added model validation for both loanRequest and UserDetails

--- ennot.py
+++ ennot.py
@@ -16,9 +16,14 @@ conn = sqlite3.connect('db.sqlite3')
 c = conn.cursor()
 
 c.execute("select * from auth_user")
-             
-
 results = c.fetchall()
+print results
+
+
 c.execute("select * from growthstreet_userdetails")
 a = c.fetchall()
-print results
+
+# create Loan Request with less than 10,000
+request = LoanRequest(reason="adf",amount=100,loanTime=2,user=users[0])
+#request.clean()
+#request.save()
--- growthstreet/models.py
+++ growthstreet/models.py
@@ -1,30 +1,9 @@
 from django.core.exceptions import ValidationError
+from django.forms import ValidationError
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-# class CompanyRegistrationNumber(models.PositiveIntegerField):
-#
-#     def __init__(self, *args, **kwargs):
-#
-#         length = kwargs.pop('length', 8)
-#         if not kwargs.get('widget'):
-#             kwargs['widget'] = forms.NumberInput(attrs={"required":"required"})
-#
-#         super(PositiveIntegerField, self).__init__(max_value=maximumvalue, min_value=minimumvalue, *args, **kwargs)
-#
-#     def to_python(self, data):
-#         return data
-#
-#      def validate(self, value):
-#
-#         super(FastaField, self).validate(value)
-#         if value != "":
-#             try:
-#                 mc.FastaFile(value, fileName=False)
-#             except ValueError as e:
-#                 raise forms.ValidationError("Sequence is not in FASTA format!")
 
 
 def RegistrationNumberValidator(value):
@@ -33,10 +12,20 @@ def RegistrationNumberValidator(value):
 
 class LoanRequest(models.Model):
     reason = models.CharField("Reason to borrow", max_length=300)
-    amount = models.PositiveIntegerField(u"Amount (\u00A3)", null=False,
-                                         validators=[MinValueValidator(10000), MaxValueValidator(100000)])
+    amount = models.PositiveIntegerField(u"Amount (\u00A3)", null=False)
+
     loanTime = models.PositiveIntegerField("Borrow time (days)")
     user = models.ForeignKey(User, null=False)
+
+    def clean(self):
+        super(LoanRequest, self).clean()
+        if self.amount < 10000 or self.amount > 100000:
+            raise ValidationError("Amount needs to be between 10000 and 100000.")
+
+    def save(self, *args, **kwargs):
+
+        self.clean()
+        super(LoanRequest, self).save(*args, **kwargs)
 
     def __str__(self):
         return "User: %s | Amount: %s | Time: %s days." % (self.user, self.amount, self.loanTime)
@@ -49,8 +38,8 @@ class LoanRequest(models.Model):
 class UserDetails(models.Model):
 
     userTelephone = models.PositiveIntegerField("Contact Number", null=False)
-    companyNumber = models.PositiveIntegerField("Company Number", null=False,
-                                                validators=[RegistrationNumberValidator])
+    companyNumber = models.PositiveIntegerField("Company Number", null=False)
+                                                #validators=[RegistrationNumberValidator])
     businessName = models.CharField("Business Name", max_length=100)
     businessAddress = models.CharField("Business Address", max_length=300)
 
@@ -63,6 +52,17 @@ class UserDetails(models.Model):
                                     choices=choices, default='Food', null=True)
 
     user = models.OneToOneField(User, null=False)
+
+    def clean(self):
+        super(UserDetails, self).clean()
+        if len(str(self.companyNumber)) != 8:
+            raise ValidationError('Registration Number has to be an 8-digit number.')
+
+
+    def save(self, *args, **kwargs):
+
+        self.clean()
+        super(UserDetails, self).save(*args, **kwargs)
 
     def __str__(self):
         return "User: %s | Contact: %s " % (self.user, self.userTelephone)
